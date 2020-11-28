85792f3f27f0a1be7c7ee65f1625f4bc1782f108
NitaRay <superlightwing@gmail.com>
2020-11-28 15:31:58 +0700

    Added dummy files to define code structure.

--- src/component/Public/Home.js
+++ src/component/Public/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import Button from '@material-ui/core/Button';
 
 class Home extends React.Component {
     render() {
         return (
-            <h1>Home</h1>
+            <div align='center'>
+                <div>
+                    <h1>Home</h1>
+                </div>
+                <div>
+                    <Button variant='contained' color='primary' component={Link}to='/login'>
+                        Hi
+                    </Button>
+                </div>
+            </div>
         )
     }
 }
