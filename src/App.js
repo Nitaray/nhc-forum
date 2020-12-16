880d6ee7c5d0bcad18ab81fb220fecd4d45cf653
Nitaray <superlightwing@gmail.com>
2020-12-16 14:37:04 +0700

    Added web title

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Register from './component/Auth/Register';
 import Home from './component/Public/Home';
 
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   const [_userToken, setToken] = useState('');
@@ -17,13 +18,18 @@ function App() {
     idHandler: setID
   }
   return (
-    <BrowserRouter basename='nhc-forum'>
-      <Switch>
-        <Route path = "/login"                          component = {() => {return (<Login session={_session}/>)}}/>
-        <Route path = "/register"                       component = {() => {return (<Register session={_session}/>)}} />
-        <Route path = "/"                               component = {() => {return (<Home session={_session}/>)}} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <Helmet>
+        <title>NHC Forum</title>
+      </Helmet>
+      <BrowserRouter basename='nhc-forum'>
+        <Switch>
+          <Route path = "/login"                          component = {() => {return (<Login session={_session}/>)}}/>
+          <Route path = "/register"                       component = {() => {return (<Register session={_session}/>)}} />
+          <Route path = "/"                               component = {() => {return (<Home session={_session}/>)}} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
